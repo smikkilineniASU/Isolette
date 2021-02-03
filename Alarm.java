cce4d9f0517a1011c29e49a6f4bb021ae1c8bcb5
smikkilineniASU <smikkil3@asu.edu>
2021-02-02 20:45:54 -0700

    Add files via upload

---
+++ Alarm.java
@@ -0,0 +1,15 @@
+package Multithreading;
+/**
+ * The alarm is turned ON by the thermostat when the current temperature drops far below the alarm low  temperature.
+ * 
+ */
+public class Alarm {
+
+	/**
+	 * Displays the Status of the alarm
+	 */
+	public void displayAlarm() {
+		System.out.println("ALARM");
+	}
+
+}
